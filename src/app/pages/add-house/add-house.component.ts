8b0e3757b595df97cb9b579fd0a6799c53a4df64
RASHID-KHAMIS <rashidkhamis168@gmail.com>
2024-05-18 15:05:52 +0300

    18/05 15:05

--- src/app/pages/add-house/add-house.component.ts
+++ src/app/pages/add-house/add-house.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-house',
   templateUrl: './add-house.component.html',
   styleUrls: ['./add-house.component.css']
 })
-export class AddHouseComponent {
+export class AddHouseComponent implements OnInit{
+
+  houseForm!:FormGroup;
+  constructor(private router:Router,
+    private route:ActivatedRoute
+  ){}
+  ngOnInit(): void {
+   this.configureForm()
+  }
+
+  configureForm(){
+    this.houseForm = new FormGroup({
+      title: new FormControl(null,Validators.required),
+      description: new FormControl(null),
+      type: new FormControl(null,Validators.required),
+      size: new FormControl(null),
+      bedrooms: new FormControl(null),
+      bathrooms: new FormControl(null),
+      address: new FormControl(null,Validators.required),
+      latitude: new FormControl(null),
+      longitude: new FormControl(null),
+      status: new FormControl(1),
+      color: new FormControl(null),
+      otherDetails: new FormControl(null)
+    })
+  }
+
+  onSave(){
+    const values = this.houseForm.value;
+    console.log(values);
+    
+  }
 
 }
