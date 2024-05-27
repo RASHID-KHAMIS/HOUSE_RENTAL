5a80724fa0980a8c91d07071df5ebe0b960e63a0
RASHID-KHAMIS <rashidkhmais168@gmail.com>
2024-05-27 12:39:09 +0300

    27/05 12:40

--- src/app/layout/house-listing/house-listing.component.ts
+++ src/app/layout/house-listing/house-listing.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HouseLocationService } from 'src/app/services/house-location.service';
 
 @Component({
   selector: 'app-house-listing',
@@ -7,11 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HouseListingComponent implements OnInit{
 
-  constructor(){}
+  constructor(private router:Router,
+    private route:ActivatedRoute,
+    private houseLocationService:HouseLocationService,
+    private sanitizer:DomSanitizer){}
   ngOnInit(): void {
-    
+    this.fetchHousePicture()
   }
 
+  houses:any;
+  imageSource1:any;
+
+  fetchHousePicture(){
+    this.houseLocationService.getAllHouseLocation().subscribe((resp:any)=>{
+      console.log(resp);
+      this.houses = resp;
+      this.imageSource1 = this.sanitizer.bypassSecurityTrustResourceUrl(`data:image/png;base64, ${resp.imageUrl}`);
+      console.log(this.imageSource1);
+      
+    })
+  }
   searchCar(){
     
   }
