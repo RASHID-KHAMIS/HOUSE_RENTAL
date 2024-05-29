dfbc0b4bf8a313a64b3fbc638b0b1c14ae829bdb
RASHID-KHAMIS <rashidkhmais168@gmail.com>
2024-05-29 09:46:45 +0300

    29/05 09:46

--- src/app/layout/house-listing/house-listing.component.ts
+++ src/app/layout/house-listing/house-listing.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HouseLocationService } from 'src/app/services/house-location.service';
+import { PriceService } from 'src/app/services/price.service';
 
 @Component({
   selector: 'app-house-listing',
@@ -13,23 +14,31 @@ export class HouseListingComponent implements OnInit{
   constructor(private router:Router,
     private route:ActivatedRoute,
     private houseLocationService:HouseLocationService,
+    private priceService:PriceService,
     private sanitizer:DomSanitizer){}
   ngOnInit(): void {
-    this.fetchHousePicture()
+    this.fetchHousePicture();
+    this.fetchAllPrice()
   }
 
   houses:any;
-  imageSource1:any;
-
   fetchHousePicture(){
     this.houseLocationService.getAllHouseLocation().subscribe((resp:any)=>{
-      console.log(resp);
       this.houses = resp;
-      this.imageSource1 = this.sanitizer.bypassSecurityTrustResourceUrl(`data:image/png;base64, ${resp.imageUrl}`);
-      console.log(this.imageSource1);
-      
     })
   }
+
+  priceInfo:any;
+  fetchAllPrice(){
+    this.priceService.getAllPrice().subscribe((resp:any)=>{
+      this.priceInfo = resp; 
+    })
+  }
+
+  displayImage(url:any){
+    return `data:image/png;base64,` + url;
+  }
+
   searchCar(){
     
   }
