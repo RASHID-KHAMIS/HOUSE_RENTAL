38d5397edc9572164358b588dd9f8c511f16f470
RASHID-KHAMIS <rashidkhmais168@gmail.com>
2024-06-11 12:38:56 +0300

    11/06 12:38

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
+
+
+  role:any;
+  constructor(private router:Router,
+    private route:ActivatedRoute){}
+  ngOnInit(): void {
+   this.role =  sessionStorage.getItem('role');
+   console.log(this.role);
+   
+  }
 
 }
