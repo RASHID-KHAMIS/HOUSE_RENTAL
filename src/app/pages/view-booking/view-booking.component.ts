07a2695f2c7a8aa0228a2874b6f5ea6bb3c3055f
RASHID-KHAMIS <rashidkhmais168@gmail.com>
2024-06-11 11:36:29 +0300

    11/06 11:36

--- src/app/pages/view-booking/view-booking.component.ts
+++ src/app/pages/view-booking/view-booking.component.ts
@@ -25,11 +25,30 @@ export class ViewBookingComponent implements OnInit{
   bookings:any
   fetchBookingInfo(id:any){
     this.houseBookingService.getByHouseBookingID(id).subscribe((resp:any)=>{
-      console.log(resp);
       this.viewBookingForm = new FormGroup({
         house_booking_id:new FormControl(resp.house_booking_id),
         house:new FormControl(resp.house),
         houseTitle:new FormControl(resp.house.title),
+        houseType:new FormControl(resp.house.type),
+        houseSize:new FormControl(resp.house.size),
+        houseBedrooms:new FormControl(resp.house.bedrooms),
+        houseBathrooms:new FormControl(resp.house.bathrooms),
+        houseAddress:new FormControl(resp.house.address),
+        otherDetails:new FormControl(resp.house.otherDetails),
+
+        customer:new FormControl(resp.customer),
+        customerFName:new FormControl(resp.customer.firstName),
+        customerLName:new FormControl(resp.customer.lastName),
+        customerEmail:new FormControl(resp.customer.email),
+        customerPhoneNumber:new FormControl(resp.customer.phoneNumber),
+        customerAddress:new FormControl(resp.customer.address),
+        customerNationalIdNumber:new FormControl(resp.customer.nationalIdNumber),
+        startDate:new FormControl(resp.startDate),
+        endDate:new FormControl(resp.endDate),
+        totalPrice:new FormControl(resp.totalPrice),
+        bookingDescription:new FormControl(resp.bookingDescription),
+        bookingNumber:new FormControl(resp.bookingNumber),
+        bookingStatus:new FormControl(1),
       })
     })
   }
@@ -48,8 +67,45 @@ export class ViewBookingComponent implements OnInit{
       staffId:new FormControl(null),
 
       houseTitle: new FormControl(null),
+      houseType: new FormControl(null),
+      houseSize: new FormControl(null),
+      houseBedrooms: new FormControl(null),
+      houseBathrooms: new FormControl(null),
+      houseAddress: new FormControl(null),
+      otherDetails: new FormControl(null),
+
+      customerFName:new FormControl(null),
+      customerLName:new FormControl(null),
+      customerEmail:new FormControl(null),
+      customerPhoneNumber:new FormControl(null),
+      customerAddress:new FormControl(null),
+      customerNationalIdNumber:new FormControl(null),
     })
   }
+
+
+  onSave(){
+    const formValues = this.viewBookingForm.value;
+
+  // Create a copy of the form values and remove the undesired fields
+  const submissionData = { ...formValues };
+  delete submissionData.customerFName;
+  delete submissionData.customerLName;
+  delete submissionData.customerEmail;
+  delete submissionData.customerPhoneNumber;
+  delete submissionData.customerAddress;
+  delete submissionData.houseTitle;
+  delete submissionData.houseType;
+  delete submissionData.houseSize;
+  delete submissionData.houseBedrooms;
+  delete submissionData. houseBathrooms;
+  delete submissionData.houseAddress;
+  delete submissionData.otherDetails;
+  delete submissionData.customerNationalIdNumber;
+
+  console.log(submissionData);
+  }
+
 
   onBack(){
 
